c33cc34a33e4e39e1ce6e900b1d0cf5e363d9cce
tutn <azjustin3@gmail.com>
2023-08-06 19:38:34 +0700

    Add role management component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /** System modules */
+  {
+    path: 'user-management',
+    loadChildren: () =>
+      import('./modules/system/user-management/user-management.module').then(
+        (m) => m.UserManagementModule
+      ),
+  },
+  {
+    path: 'role-management',
+    loadChildren: () =>
+      import('./modules/system/role-management/role-management.module').then(
+        (m) => m.RoleManagementModule
+      ),
+  },
+  /**  */
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoleManagementComponent } from './modules/system/role-management/role-management.component';
+import { UserManagementComponent } from './modules/system/user-management/user-management.component';
 import { UserManagementModule } from './modules/system/user-management/user-management.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { MaterialModule } from './shared/material/material.module';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 
 const SHARE_MODULES = [MaterialModule];
 
-const SHARE_COMPONENTS = [AppComponent, HeaderComponent];
+const SHARE_COMPONENTS = [AppComponent, HeaderComponent, NavigationComponent];
+const SYSTEM_COMPONENTS = [UserManagementComponent, RoleManagementComponent];
 
 @NgModule({
-  declarations: [...SHARE_COMPONENTS],
+  declarations: [...SHARE_COMPONENTS, ...SYSTEM_COMPONENTS],
   imports: [
     ...SHARE_MODULES,
+    RouterModule.forRoot([]),
     BrowserModule,
     AppRoutingModule,
     UserManagementModule,
