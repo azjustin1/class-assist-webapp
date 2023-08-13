742bbd97bd68921ecd7043eaade592223be34d63
tutn <azjustin3@gmail.com>
2023-08-14 01:20:33 +0700

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,33 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UserManagementModule } from './modules/system/user-management/user-management.module';
-import { HeaderModule } from './shared/header/header.module';
-import { MaterialModule } from './shared/material/material.module';
-import { NavigationComponent } from './shared/navigation/navigation.component';
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { UserManagementModule } from './modules/system/user-management/user-management.module'
+import { AuthProvider } from './shared/auth/auth.interceptor'
+import { HeaderModule } from './shared/header/header.module'
+import { MaterialModule } from './shared/material/material.module'
+import { NavigationComponent } from './shared/navigation/navigation.component'
 
-const SHARE_MODULES = [MaterialModule, FormsModule, HeaderModule];
+const SHARE_MODULES = [MaterialModule, FormsModule, HeaderModule]
 
-const SHARE_COMPONENTS = [AppComponent, NavigationComponent];
-const SYSTEM_MODULES = [UserManagementModule];
+const SHARE_COMPONENTS = [AppComponent, NavigationComponent]
+const SYSTEM_MODULES = [UserManagementModule]
 
 @NgModule({
-  declarations: [...SHARE_COMPONENTS],
-  imports: [
-    ...SHARE_MODULES,
-    ...SYSTEM_MODULES,
-    RouterModule.forRoot([]),
-    BrowserModule,
-    AppRoutingModule,
-    UserManagementModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [...SHARE_COMPONENTS],
+	imports: [
+		...SHARE_MODULES,
+		...SYSTEM_MODULES,
+		RouterModule.forRoot([]),
+		BrowserModule,
+		AppRoutingModule,
+		UserManagementModule,
+		BrowserAnimationsModule,
+	],
+	providers: [AuthProvider],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 export interface MenuItem {
   name: string;
@@ -17,5 +16,12 @@ export class NavigationComponent {
     { name: 'Role management', path: '/role-management' },
   ];
 
+  @Output() onMenuItemClick = new EventEmitter();
+
   constructor() {}
+
+
+  onNavigate() {
+    this.onMenuItemClick.emit();
+  }
 }
