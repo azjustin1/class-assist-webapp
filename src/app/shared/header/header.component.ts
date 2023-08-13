181989f39614831c5307e7f3d24fb5a90099a889
tutn <azjustin3@gmail.com>
2023-08-14 01:18:59 +0700

    add select language

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,15 +1,42 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import { Component, EventEmitter, Output } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { Router } from '@angular/router'
+import { LoginDialogComponent } from '../dialogs/login-dialog/login-dialog.component'
+import { HeaderService } from './header.service'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  @Output() sidenav = new EventEmitter();
+	@Output() sidenav = new EventEmitter()
 
-  toggleSidenav() {
-    this.sidenav.emit();
-  }
+	constructor(
+		private router: Router,
+		private httpClient: HttpClient,
+		private dialog: MatDialog,
+		private headerService: HeaderService
+	) {}
+
+	toggleSidenav() {
+		this.sidenav.emit()
+	}
+
+	goToHome() {
+		this.router.navigateByUrl('/')
+	}
+
+	openLoginDialog(): void {
+		const dialogRef = this.dialog.open(LoginDialogComponent, {})
+
+		dialogRef.afterClosed().subscribe((result) => {})
+	}
+
+	test() {
+		this.headerService.getUser().subscribe((res) => {
+			console.log(res)
+		})
+	}
 }
