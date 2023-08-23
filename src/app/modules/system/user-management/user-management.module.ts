1bf978c119a26702d1005307c6c083829274b687
tutn <azjustin3@gmail.com>
2023-08-24 00:06:17 +0700

    Add user table

--- src/app/modules/system/user-management/user-management.module.ts
+++ src/app/modules/system/user-management/user-management.module.ts
@@ -1,9 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { UserManagementRoutingModule } from './user-management-routing.module';
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+import { UserManagementRoutingModule } from './user-management-routing.module'
+import { UserManagementComponent } from './user-management.component'
+import { MaterialModule } from 'src/app/shared/material/material.module'
+import { UserManagementService } from './user-management.service'
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, UserManagementRoutingModule],
+	declarations: [UserManagementComponent],
+	imports: [CommonModule, UserManagementRoutingModule, MaterialModule],
+	providers: [UserManagementService],
 })
 export class UserManagementModule {}
---
+++ src/app/modules/system/user-management/user-management.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { User } from './models/user.model'
+
+@Injectable()
+export class UserManagementService {
+	constructor(private httpClient: HttpClient) {}
+
+	getAllUsers(): Observable<User[]> {
+		return this.httpClient.get<User[]>('rest/users')
+	}
+}
